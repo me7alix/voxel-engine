d4dac4ef2a16149eddc4d9ec59b56cb4ae4f1932
Proger-3301 <proger3301@gmail.com>
2024-09-27 19:06:10 +0300

    remove build folder

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/9E4668B146688BB9/CProjects/voxel-engine
-# Build directory: /mnt/9E4668B146688BB9/CProjects/voxel-engine/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
